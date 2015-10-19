c5612a76552bebe18709c03d3bcec77bcf77e4b9
kakashinss <goodtutohd@gmail.com>
2015-10-19 14:34:47 -0200

    Patch 0.0.2
    Galinha spawnando randomicamente e andando.
    Bug do chão corrigido.
    Parede para as galinhas n seguirem adiante.

---
+++ GalinhaGame/Assets/Scripts/Galinha.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Galinha : MonoBehaviour {
+
+    public GameObject spawn1;
+    public GameObject spawn2;
+    public GameObject spawn3;
+    public GameObject galinha;
+    private int rand;
+    private float timer = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        timer += Time.deltaTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        Debug.Log(timer);
+        if (timer > 3)
+        {
+
+            rand = Random.Range(0, 3);
+            if (rand == 0)
+            {
+                BoxCollider cloneGalinha;
+                cloneGalinha = Instantiate(galinha, spawn1.transform.position, spawn1.transform.rotation) as BoxCollider;
+            }
+            else
+            if (rand == 1)
+            {
+                BoxCollider cloneGalinha;
+                cloneGalinha = Instantiate(galinha, spawn2.transform.position, spawn2.transform.rotation) as BoxCollider;
+            }
+            else
+            if (rand == 2)
+            {
+                BoxCollider cloneGalinha;
+                cloneGalinha = Instantiate(galinha, spawn3.transform.position, spawn3.transform.rotation) as BoxCollider;
+            }
+            timer = 0;
+
+
+        }
+        
+
+
+        //Rigidbody cloneGalinha;
+        //cloneGalinha = Instantiate(galinha, spawn1.transform.position, spawn1.transform.rotation)as Rigidbody;
+
+
+    }
+
+}
---
+++ GalinhaGame/Assets/Scripts/GalinhaWalk.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GalinhaWalk : MonoBehaviour {
+    
+    public float speedMove;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(Vector3.forward * speedMove);
+
+
+    }
+}
