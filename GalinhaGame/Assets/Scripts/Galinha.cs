6c5580b492c22058b263e7333035d47877124a10
kakashinss <goodtutohd@gmail.com>
2015-10-20 19:25:10 -0200

    Patch 0.0.5
    Tempo pra galinha morrer.

--- GalinhaGame/Assets/Scripts/Galinha.cs
+++ GalinhaGame/Assets/Scripts/Galinha.cs
@@ -9,6 +9,8 @@ public class Galinha : MonoBehaviour {
     public GameObject galinha;
     private int rand;
     private float timer = 0;
+    private float timer2 = 0;
+    private GameObject cloneGalinha;
 
     // Use this for initialization
     void Start () {
@@ -18,38 +20,86 @@ public class Galinha : MonoBehaviour {
     void FixedUpdate ()
     {
         timer += Time.deltaTime;
+        timer2 += Time.deltaTime;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
-        Debug.Log(timer);
-        if (timer > 3)
-        {
 
+    // Update is called once per frame
+    void Update() {
+
+        if (timer > 6) {
             rand = Random.Range(0, 3);
+            //ponto 1
             if (rand == 0)
             {
-                BoxCollider cloneGalinha;
-                cloneGalinha = Instantiate(galinha, spawn1.transform.position, spawn1.transform.rotation) as BoxCollider;
-            }
-            else
-            if (rand == 1)
-            {
-                BoxCollider cloneGalinha;
-                cloneGalinha = Instantiate(galinha, spawn2.transform.position, spawn2.transform.rotation) as BoxCollider;
-            }
-            else
-            if (rand == 2)
-            {
-                BoxCollider cloneGalinha;
-                cloneGalinha = Instantiate(galinha, spawn3.transform.position, spawn3.transform.rotation) as BoxCollider;
+                Debug.Log(timer);
+                if (timer > 6)
+                {
+
+                    cloneGalinha = Instantiate(galinha, spawn1.transform.position, spawn1.transform.rotation) as GameObject;
+                    
+
+                }
+
+                if (timer2 > 12)
+                {
+
+
+                    Destroy(cloneGalinha);
+                    timer2 = 6;
+
+                }
             }
             timer = 0;
+            //ponto 2
+            if (rand == 1)
+            {
+                Debug.Log(timer);
+                if (timer > 6)
+                {
+
+                    cloneGalinha = Instantiate(galinha, spawn2.transform.position, spawn2.transform.rotation) as GameObject;
+
+                }
+
+                if (timer2 > 12)
+                {
 
 
+                    Destroy(cloneGalinha);
+                    timer2 = 6;
+
+                }
+            }
+            timer = 0;
+            //ponto 3
+            if (rand == 2)
+            {
+                Debug.Log(timer);
+                if (timer > 6)
+                {
+
+                    cloneGalinha = Instantiate(galinha, spawn3.transform.position, spawn3.transform.rotation) as GameObject;
+
+                }
+
+                if (timer2 > 12)
+                {
+
+
+                    Destroy(cloneGalinha);
+                    timer2 = 6;
+
+                }
+            }
+            timer = 0;
         }
-        
+
+
+
+
+
+
+
 
 
         //Rigidbody cloneGalinha;
--- GalinhaGame/Assets/Scripts/Perdeu.cs
+++ GalinhaGame/Assets/Scripts/Perdeu.cs
@@ -3,24 +3,25 @@ using System.Collections;
 
 public class Perdeu : MonoBehaviour {
 
-    public GameObject galinha;
-    public GameObject parede;
+  
 
     // Use this for initialization
     void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 	    
+       
 
 	}
 
-    void OnCollisionEnter(Collision parede)
-    {
-        Debug.Log("Adentrou");
-        //pontos++;
-        DestroyObject(galinha);
-    }
+    
+
 }
