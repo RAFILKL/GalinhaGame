148b7cedb737d4f479d6851530d77ad4d6d5518f
NicolasCampana <beck_ram@hotmail.com>
2016-01-07 22:37:38 -0200

    Patch 0.6
    - > Todas as animações completas

--- GalinhaGame/Assets/Scripts/GalinhaWalk.cs
+++ GalinhaGame/Assets/Scripts/GalinhaWalk.cs
@@ -1,201 +1,269 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GalinhaWalk : MonoBehaviour {
-    
-    
-	public int rand, rand2,rand3;
-	private int  andar, ciscar,voar, cair;
-	public Animator galinhaAnimator;
-	public Rigidbody rigGalinha;
-	public Animation galinhaAnimation;    
-	public float timer, vel;
-	public static bool parada, jaPassou, especial;
-	public static int valorGalinha;
-	public Vector3 velc = new Vector3(0,0,1);
+public class GalinhaWalk : MonoBehaviour
+{
+
+
+    public int rand, rand2, rand3;
+    private int andar, ciscar, voar;
+    public Animator galinhaAnimator;
+    public Animation galinhaAnimation;
+    public float timer, vel, velSpeed;
+    public bool parada, jaPassou, especial;
+    public static bool outraParada = false;
+    public static int valorGalinha;
+    public Vector3 velc = new Vector3(0, 0, 1);
+
+    const int Easy = 2;
+    const int Medium = 3;
+    const int Hard = 4;
+    const int Nivel = Easy;
 
     // Use this for initialization
 
-	void Awake() {
-		 jaPassou = false;
-		 parada = false;
-		 especial = false;
-	}
-    void Start () {
-		andar = Animator.StringToHash ("andar");
-		ciscar = Animator.StringToHash ("ciscar");
-		voar = Animator.StringToHash ("voar");
-		cair = Animator.StringToHash ("cair");
-		//Ativa a animaçao da galinha para andar
-		galinhaAnimator.SetBool(andar,true);
-		galinhaAnimator.SetBool(ciscar,false);
-		rigGalinha = GetComponentInChildren<Rigidbody> ();
-		rand = Random.Range(1,8);
+    void Awake()
+    {
+        jaPassou = false;
+        parada = false;
+        especial = false;
+        //outraParada = false;
+        flySpeed = 0;
+    }
+    void Start()
+    {
+        andar = Animator.StringToHash("andar");
+        ciscar = Animator.StringToHash("ciscar");
+        voar = Animator.StringToHash("voar");
+        //Ativa a animaçao da galinha para andar
+        galinhaAnimator.SetBool(andar, true);
+        galinhaAnimator.SetBool(ciscar, false);
+        rand = Random.Range(1, 8);
 
-	}
-	void FixedUpdate() {
-		timer += Time.deltaTime;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (parada) {
-		} else {
-			galinhaAnda ();
-		}
-	}
-	public void testaTrigger(){
-		rand2 = Random.Range (4,5);
-		// 20% de chance da galinha 
-		switch (rand2) {
-		case 0:
-			break;
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			if(!especial) {
-			  this.mudaGalinha();
-			  especial = true;
-			}
-			break;
-		}
-	
-	}
-	public void galinhaAnda() {
-		switch (rand) {
-		case 1:
-			if (parada == false && jaPassou == false) {
-				vel = 0.3f;
-				valorGalinha = 30;
-				this.mudaVelocidade (vel);
-				// jaPassou = true;
-			}
-			break;
-		case 2:
-			if (parada == false && jaPassou == false) {
-				vel = 0.5f;
-				valorGalinha = 30;
-				this.mudaVelocidade (vel);
-				//	jaPassou = true;
-			}
-			break;
-		case 3:
-			if (parada == false && jaPassou == false) {
-				vel = 0.7f;
-				valorGalinha = 30;
-				this.mudaVelocidade (vel);
-				//	jaPassou = true;
-			}
-			break;
-		case 4:
-			if (parada == false && jaPassou == false) {
-				vel = 0.9f;
-				valorGalinha = 35;
-				this.mudaVelocidade (vel);
-				//	jaPassou = true;
-			}
-			break;
-		case 5:
-			if (parada == false && jaPassou == false) {
-				vel = 1f;
-				valorGalinha = 45;
-				this.mudaVelocidade (vel);
-				//	jaPassou = true;
-			}
-			break;
-		case 6:
-			if (parada == false && jaPassou == false) {
-				vel = 1.2f;			
-				valorGalinha = 55;
-				this.mudaVelocidade (vel);
-				//	jaPassou = true;
-			}
-			break;
-		case 7:
-			if (parada == false && jaPassou == false) {
-				vel = 1.3f;
-				valorGalinha = 65;
-				this.mudaVelocidade (vel);
-				//	jaPassou = true;
-			}
-			break;
-		case 8:
-			if (parada == false && jaPassou == false) {
-				vel = 1.5f;
-				valorGalinha = 75;
-				this.mudaVelocidade (vel);
-				jaPassou = true;
-			}
-			break;
-		default:
-			break;
-		}	
-	}
+    }
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+    }
 
-		public void paraGalinha() {	
-		   parada = true;
-		   this.transform.Translate (0,0,0);		   
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        if (!parada)
+        {
+            galinhaAnda();
+        }
+    }
+    public void testaTrigger()
+    {
+        rand2 = Random.Range(0, 5);
 
+        // 20% de chance da galinha 
+#if UNITY_EDITOR
+        if(!outraParada && !especial)
+//#else
+        if (rand2 == 4  && !outraParada && !especial)
+#endif
+        {
+            outraParada = true;
+            mudaGalinha();
+            Debug.Log("GAlinha especial!");
+            this.especial = true;
+        }
+    }
 
+    IEnumerator AguardaGalinha()
+    {
+        parada = true;
+        Debug.LogFormat("Galinha {0} parou!", gameObject.name);
+        yield return new WaitForSeconds(2f);
+        Debug.LogFormat("Galinha {0} andou!", gameObject.name);
+        parada = false;
+        outraParada = false;
+        galinhaAnimator.SetBool(ciscar, false);
+        galinhaAnimator.SetBool(andar, true);
+    }
 
-		public void mudaVelocidade(float vel) {	
-			if(!parada){
-		    float vel2 = vel + 0.4f;			
-		    this.transform.Translate (new Vector3(0,0,1) * vel);		    
-			galinhaAnimator.speed = vel2;
-		    } else {
-				this.transform.Translate (velc * 0);		    
-				galinhaAnimator.speed = 0;
-			}
-			
-		}
-	private void voaGalinha(){
-		galinhaAnimator.SetBool (voar, true);
-		galinhaAnimator.SetBool (andar,false);
-	
-		//transform.Translate (Vector3.up * vel);
-	}
-	private void ciscaGalinha() {
-		this.paraGalinha ();
-		galinhaAnimator.SetBool (ciscar, true);
-		galinhaAnimator.SetBool(andar,false);
+    public void galinhaAnda()
+    {
+        outraParada = false;
+        switch (rand)
+        {
+            case 1:
+                if (!parada)
+                {
+                    vel = 0.3f;
+                    valorGalinha = 30;
+                    this.mudaVelocidade(vel);
+                }
+                break;
+            case 2:
+                if (!parada)
+                {
+                    vel = 0.5f;
+                    valorGalinha = 30;
+                    this.mudaVelocidade(vel);
+                }
+                break;
+            case 3:
+                if (!parada)
+                {
+                    vel = 0.7f;
+                    valorGalinha = 30;
+                    this.mudaVelocidade(vel);
 
-	}
-	private void aceleraGalinha() {
-		vel += 0.2f;
-		this.mudaVelocidade (vel);
-
-	}
-	void mudaGalinha() {
-		rand3 = Random.Range(0,3);
-		switch (rand3) {
-	    //Galinha acelera
-		case 0:
-			this.aceleraGalinha();
-			break;
-		//Galinha voa
-		case 1:
-			this.voaGalinha();
-			break;
-		//Galinha cisca
-		case 2:
-			this.ciscaGalinha();
-			break;		
-		
-		}
-
-	}
-
-	void OnTriggerEnter() {
-		this.testaTrigger ();
-	}
+                }
+                break;
+            case 4:
+                if (!parada)
+                {
+                    vel = 0.9f;
+                    valorGalinha = 35;
+                    this.mudaVelocidade(vel);
+                }
+                break;
+            case 5:
+                if (!parada)
+                {
+                    vel = 1f;
+                    valorGalinha = 45;
+                    this.mudaVelocidade(vel);
+                }
+                break;
+            case 6:
+                if (!parada)
+                {
+                    vel = 1.2f;
+                    valorGalinha = 55;
+                    this.mudaVelocidade(vel);
+                }
+                break;
+            case 7:
+                if (!parada)
+                {
+                    vel = 1.3f;
+                    valorGalinha = 65;
+                    this.mudaVelocidade(vel);
+                }
+                break;
+            case 8:
+                if (!parada)
+                {
+                    vel = 1.5f;
+                    valorGalinha = 75;
+                    this.mudaVelocidade(vel);
+                }
+                break;
+        }
+    }
 
 
-	
+    public void paraGalinha()
+    {
+        foreach (GalinhaWalk Galinha in FindObjectsOfType<GalinhaWalk>())
+        {
+            if (Galinha.GetInstanceID() != this.GetInstanceID())
+            {
+                //Galinha.transform.Translate(Vector3.zero);
+                //Galinha.parada = true;
+                if (Galinha.parada)
+                {
+                    Galinha.parada = false;
+                  
+                }
+            }
+        }
+
+        especial = true;
+        parada = true;
+    }
+
+
+    public float multiplier = 1f;
+    public void mudaVelocidade(float vel)
+    {
+        float vel2 = (vel + 0.4f) * multiplier * Time.deltaTime;
+        this.transform.Translate(velc * vel);
+        galinhaAnimator.speed = vel2;
+
+
+
+    }
+    IEnumerator VoaGalinha()
+    {
+        flySpeed = 0;
+        for (int i = 1; i < 11; i++)
+        {
+            flySpeed += i * 7;
+            transform.Translate(Vector3.up * flySpeed * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return new WaitForSeconds(1f);
+        galinhaAnimator.SetBool(voar, false);
+        galinhaAnimator.SetBool(andar, true);
+    }
+
+    public float flySpeed = 1f;
+    public void voaGalinha()
+    {        
+        parada = true;
+        galinhaAnimator.speed = 0.05f;
+        galinhaAnimator.SetBool(voar, true);
+        galinhaAnimator.SetBool(andar, false);
+        StartCoroutine(VoaGalinha());
+        parada = false;
+    }
+  
+    void ciscaGalinha()
+    {
+        StartCoroutine(AguardaGalinha());
+        paraGalinha();
+        galinhaAnimator.SetBool(ciscar, true);
+        galinhaAnimator.SetBool(andar, false);
+    }
+    IEnumerator AceleraGalinha()
+    {
+        velSpeed = vel;
+        parada = true;
+        for (int i = 1; i < 30; i++)
+        {
+           
+            velSpeed += 0.15f;          
+            this.transform.Translate(velc * velSpeed);
+            galinhaAnimator.speed = velSpeed;
+            yield return new WaitForSeconds(0.01f);
+        }
+        parada = false;
+    }
+
+
+    void mudaGalinha()
+    {
+        rand3 = Random.Range(0, 3);
+        switch (rand3)
+        {
+            //Galinha acelera
+            case 0:
+                StartCoroutine(AceleraGalinha());
+                break;
+            //Galinha voa
+            case 1:
+                Debug.Log("VOou");
+                voaGalinha();
+                break;
+            //Galinha cisca
+            case 2:
+                ciscaGalinha();
+                break;
+
+        }
+    }
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "Galinha")
+        {
+            transform.Translate(1.5f,0,0);
+            col.gameObject.transform.Translate(1.5f, 0, 0);
+                
+        }
+    }
 }
-
--- GalinhaGame/Assets/Scripts/TriggerController.cs
+++ GalinhaGame/Assets/Scripts/TriggerController.cs
@@ -5,11 +5,17 @@ public class TriggerController : MonoBehaviour {
 
 	public GameObject gal;
 
-
-	void Start() {
+	void Start() 
+    {
 	    
 	}
 
-
-
+    void OnTriggerEnter(Collider objectCollider)
+    {
+        GalinhaWalk Galinha = objectCollider.gameObject.GetComponent<GalinhaWalk>();
+        if (Galinha != null) {
+            Debug.LogFormat("Galinha {0} entrou no trigger {1}", Galinha.gameObject.name, gameObject.name);           
+            Galinha.testaTrigger();
+        }       
+    }
 }
